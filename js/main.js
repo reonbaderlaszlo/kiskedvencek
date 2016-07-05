67a26950018f68f3bca6f3083697a2bf8690de67
Báder László <bader.laszlo22@gmail.com>
2016-07-05 14:32:50 +0200

    Roadshow done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 $(function(){
     
+	$('.pictures a').on('click', function (e) {
+		 
+		e.preventDefault();
+
+		$('.overlay').fadeIn('fast');
+		$('.gallery').fadeIn('fast');
+
+	});
+
+	$('.gallery .close').on('click', function (e) {
+
+		e.preventDefault();
+		$('.overlay').fadeOut('fast');
+		$('.gallery').fadeOut('fast');
+
+	});
+
+	$('.overlay').on('click', function (e) {
+
+		e.preventDefault();
+		$('.overlay').fadeOut('fast');
+		$('.gallery').fadeOut('fast');
+
+	});
+
+	$('.pictures a').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+	    }, 500);
+	    return false;
+	});
+
 });
