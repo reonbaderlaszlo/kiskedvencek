f5d1b8dccb0e63714d0827ca33a490fb5a7c1b4c
Báder László <bader.laszlo22@gmail.com>
2016-07-06 15:59:01 +0200

    partners almost done

--- js/main.js
+++ js/main.js
@@ -1,35 +1,70 @@
-$(function(){
-    
-	$('.pictures a').on('click', function (e) {
-		 
-		e.preventDefault();
+$(function() {
 
-		$('.overlay').fadeIn('fast');
-		$('.gallery').fadeIn('fast');
+    $('.pictures a').on('click', function(e) {
 
-	});
+        e.preventDefault();
 
-	$('.gallery .close').on('click', function (e) {
+        $('.overlay').fadeIn('fast');
+        $('.gallery').fadeIn('fast');
 
-		e.preventDefault();
-		$('.overlay').fadeOut('fast');
-		$('.gallery').fadeOut('fast');
+    });
 
-	});
+    $('.gallery .close').on('click', function(e) {
 
-	$('.overlay').on('click', function (e) {
+        e.preventDefault();
+        $('.overlay').fadeOut('fast');
+        $('.gallery').fadeOut('fast');
 
-		e.preventDefault();
-		$('.overlay').fadeOut('fast');
-		$('.gallery').fadeOut('fast');
+    });
 
-	});
+    $('.overlay').on('click', function(e) {
 
-	$('.pictures a').click(function(){
-	    $('html, body').animate({
-	        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
-	    }, 500);
-	    return false;
-	});
+        e.preventDefault();
+        $('.overlay').fadeOut('fast');
+        $('.gallery').fadeOut('fast');
+
+    });
+
+    $('.pictures a').click(function() {
+        $('html, body').animate({
+            scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+        }, 500);
+        return false;
+    });
+
+    $('.show-password').on('click', function(e) {
+
+        e.preventDefault();
+
+
+        if ($(this).hasClass('active')) {
+
+            $(this).removeClass('active');
+
+            var text_val = $(this).parent().find('input:text').val();
+
+            $(this).parent().find('input:text').hide();
+
+            $(this).parent().find('input:password').val(text_val);
+
+            $(this).parent().find('input:password').show();
+
+
+
+        } else {
+
+            $(this).addClass('active');
+
+            var pw_val = $(this).parent().find('input:password').val();
+
+            $(this).parent().find('input:password').hide();
+
+            $(this).parent().find('input:text').val(pw_val);
+
+            $(this).parent().find('input:text').show();
+
+        }
+
+    });
 
 });
